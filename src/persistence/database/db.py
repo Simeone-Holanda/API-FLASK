495fa78f2bbedfe207887d2b74b3d94007f4ac9a
Simeone <simeone.10s10@gmail.com>
2021-05-20 19:15:14 -0300

    Created database version1.

---
+++ src/persistence/database/db.py
@@ -0,0 +1,75 @@
+from pymongo import MongoClient
+from bson.json_util import ObjectId
+
+class Database:
+    name_collection='Datas'
+    connection = None
+    def __init__(self):
+        pass
+
+    # IMPLEMENTAR A ESTRATÉGIA DE DEFINE COLLECTION ?
+    def set_connection(self):
+        try:
+            client_connection = MongoClient("mongodb://127.0.0.1:27017/")
+            db = client_connection['Users']
+            self.connection = db[self.name_collection]
+        except Exception as ex:
+            raise ex
+        else:
+             print("Connection created.")
+    
+    def insert(self, documento_user: dict):
+        try:
+            self.connection.insert_one(documento_user)
+        except Exception as ex:
+            raise ex
+
+    def update_user(self,user_id,newDocumento):
+        try:
+            response = self.connection.find_one_and_update(
+                {'_id': ObjectId(user_id)},
+                 {'$set': newDocumento},
+                return_document=True)
+        except Exception as ex:
+            raise ex
+        else:
+            if response is None:
+                raise Exception('Unexpected value.')
+            else:
+                return response
+    
+    def find_user(self,user_id):
+        try:
+            response = self.connection.find_one({'_id': ObjectId(user_id)})
+        except Exception as ex:
+            raise ex
+        else:
+            return response
+
+    def find_all_users(self):
+        try:
+            response = self.connection.find()
+        except Exception as ex:
+            raise ex
+        else:
+            if response is not None:
+                return list(response)
+            else:
+                print("There is no data in the database.")
+                return response
+
+    def find_login(self, login, password):
+        try:
+            response = self.connection.find_one({'login': login, 'senha': password}) 
+        except Exception as ex:
+            raise ex
+        else:
+            return response
+
+    def delete_user(self, user_id):
+        try:
+            response = self.connection.find_one_and_delete({'_id': ObjectId(user_id)})
+        except Exception as ex:
+            raise ex
+
+       
